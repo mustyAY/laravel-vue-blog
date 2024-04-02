404e479a8b92c7fbdce0401c2c31718b2a49a446
Ahmad Busari <bahmad@schooltry.com>
2024-04-02 11:48:48 +0100

    seperated stack

--- api/routes/api.php
+++ api/routes/api.php
@@ -8,6 +8,4 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts', [PostController::class, 'show']);
-
 require __DIR__.'/post.php';
--- resources/js/app.js
+++
@@ -1,8 +0,0 @@
-import './bootstrap';
-
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router/routes';
-
-createApp(App).use(router).mount("#app");
-
